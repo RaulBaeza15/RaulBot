c76432b78b5365c0a9218cc3d8806edce8ccc790
RaulBaeza15 <95641914+RaulBaeza15@users.noreply.github.com>
2022-05-17 20:44:30 +0200

    Cortadora

--- src/Run.java
+++ src/Run.java
@@ -1,5 +1,7 @@
 public class Run {
     public static void main(String[] args) {
-        
+        for (String silaba: Cortadora.getSilabas()             ) {
+            System.out.println(silaba);
+        }
     }
 }
